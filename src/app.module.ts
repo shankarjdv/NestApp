765b14d022630681dfea2afb743069b01ff29265
Shankar <shankarjadhav4177@gmail.com>
2025-01-10 07:38:53 +0530

    folder structure created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CampaignModule } from './campaign/campaign.module';
 
 @Module({
-  imports: [],
+  imports: [CampaignModule],
   controllers: [AppController],
   providers: [AppService],
 })
